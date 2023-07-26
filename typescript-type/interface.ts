12e0d2543d0d2b659d4e30ffbb566d7b86b7036d
sgmikami03 <sgmikami03@gmail.com>
2023-07-27 07:48:07 +0900

    [add]63

--- typescript-type/interface.ts
+++ typescript-type/interface.ts
@@ -2,11 +2,14 @@
 interface Human {
     name: string;
     age: number;
+    greeting: (message: string) => void;
 }
 
 const human: Human = {
     name: "quill",
     age: 38,
+    greeting(message: string){
+        console.log(message);
+        
+    }
 }
-
-let developer: Human;
