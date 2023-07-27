549c61a1f63ebb23a6c67239c3ddfda1a5185787
sgmikami03 <sgmikami03@gmail.com>
2023-07-28 07:51:53 +0900

    [add]-70

--- typescript-type/interface.ts
+++ typescript-type/interface.ts
@@ -1,5 +1,25 @@
+// type addFunc = (num1: number, num2: number) => number;
 
-interface Human {
+interface addFunc {
+    (num1: number, num2: number): number;
+}
+
+let addFunc: addFunc;
+addFunc = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
+const nameable: Namable = {
+    name: "quill",
+    nickName: "Quiita",
+}
+
+interface Namable {
+    name: string;
+    nickName?: string;
+}
+
+interface Human extends Namable {
     name: string;
     age: number;
     greeting: (message: string) => void;
@@ -10,6 +30,21 @@ const human: Human = {
     age: 38,
     greeting(message: string){
         console.log(message);
-        
     }
 }
+
+class Developer implements Human {
+    constructor(public name: string, public age: number, public experience: string) {}
+    greeting(message: string) {
+        console.log("hello");
+    }
+}
+const temDeveloper = {
+    name: "quill",
+    age: 38,
+    experience: "keireki",
+    greeting(message: string) {
+        console.log("hello");
+    }
+}
+const user: Human = temDeveloper;
